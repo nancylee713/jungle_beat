21751ce8ee835ebcad95fbdf72bb87c9261ab3b5
Nancy Lee <srlee713@gmail.com>
2019-09-28 11:22:11 -0600

    Initial commit

---
+++ lib/node.rb
@@ -0,0 +1,7 @@
+class Node
+  attr_reader :sound
+  
+  def initialize(sound)
+    @sound = sound
+  end
+end
---
+++ test/node_test.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/node'
+
+class NodeTest < Minitest::Test
+end
