49d7803cf701e63030feaadd9e00699f3ae41ba2
Nancy Lee <srlee713@gmail.com>
2019-09-28 11:25:29 -0600

    Add existence test

--- test/node_test.rb
+++ test/node_test.rb
@@ -3,4 +3,8 @@ require 'minitest/pride'
 require './lib/node'
 
 class NodeTest < Minitest::Test
+  def test_new_returns_a_node
+    obj = Node.new("plop")
+    assert_instance_of Node, obj
+  end
 end
